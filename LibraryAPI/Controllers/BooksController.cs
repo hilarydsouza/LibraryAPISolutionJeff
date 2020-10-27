ea3b9660b05055ccbb168a7ee4d2855bec14adb9
hilarydsouza <48483928+hilarydsouza@users.noreply.github.com>
2020-10-27 16:07:11 -0400

    Initial commit

---
+++ LibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using LibraryAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Controllers
+{
+    public class BooksController: ControllerBase
+    {
+
+        private readonly LibraryDataContext _context;
+
+        public BooksController(LibraryDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("books")]
+        public async Task<ActionResult> GetAllBooks()
+        {
+            var response = await _context.Books.ToListAsync();
+            return Ok(response);
+        }
+       
+    }
+}
